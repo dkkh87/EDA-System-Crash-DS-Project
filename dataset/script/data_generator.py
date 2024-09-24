cf51cd3eb1891b745bc220f5934600d4044c54bc
Deepak Khandelwal <dekhandelwal@microsoft.com>
2024-09-24 06:43:59 -0700

    increasng dataset size and duplicate rwos

--- dataset/script/data_generator.py
+++ dataset/script/data_generator.py
@@ -5,13 +5,13 @@ import numpy as np
 np.random.seed(45)
  
 # Parameters
-num_rows = 100000
+num_rows = 1000000
 
 # 4 CORES, 8 GB RAM, 1Gbps (1000MB) 
 # Generate random data
 data = {
     'Device ID': range(1, num_rows + 1),
-    'CPU Usage (%)': np.array([np.random.randint(101, 300) if i % 4000 == 0 else "" if i % 6000 == 0 else np.random.randint(20, 100) for i in range(num_rows)]),
+    'CPU Usage (%)': np.array([np.round(np.random.uniform(101, 300), 3) if i % 4000 == 0 else "" if i % 6000 == 0 else np.round(np.random.uniform(20, 100), 3) for i in range(num_rows)]),
     'Memory Usage (MB)': np.array([np.random.randint(8500, 10000) if i % 4000 == 0 else np.random.randint(500, 8000) for i in range(num_rows)]),
     'Disk Usage (%)': np.random.randint(10, 100, size=num_rows),
     'Network Activity (MB/s)': np.random.randint(0, 500, size=num_rows),
@@ -23,7 +23,7 @@ data = {
     'GPU Usage (%)': np.array([np.random.randint(101, 500) if i % 2500 == 0 else "" if i % 1200 == 0 else np.random.randint(0, 100) for i in range(num_rows)]),
     'Temperature (°C)': np.array([np.random.randint(150, 200) if i % 6000 == 0 else np.random.randint(30, 100) for i in range(num_rows)]),
     'Storage Available (GB)': np.random.randint(0, 500, size=num_rows),
-    'Recent App': np.random.choice(['App A', 'App B', 'App C', 'App D', 'App E', 'App F', 'App G', 'App H', 'App I', 'App J'], size=num_rows),
+    'Recent App': np.random.choice(['Amazon', 'Spotify', 'Netflix', 'Youtube', 'Tiktok', 'Snapchat', 'Twitter', 'Instagram', 'WhatsApp', 'Facebook', 'Audible', 'Candy Crush Saga', 'Signal', 'Reddit'], size=num_rows),
     'Screen Brightness (%)': np.random.randint(0, 100, size=num_rows),
     'User Activity Level': np.random.choice(['Low', 'Medium', 'High'], size=num_rows),
 
@@ -42,7 +42,10 @@ df['System Crash (Yes/No)'] = np.where(
     'Yes',
     'No'
 )
- 
+
+random_rows = df.sample(n=1000, random_state=25)
+df = df.append(random_rows, ignore_index=True)
+
 # Save to CSV
 df.to_csv('device_crash_dataset.csv', index=False)
  
